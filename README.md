91505ad4596ff7be8b0475046497c3c42bbb400e
Lendoly <luis.hdezdonadeu@outlook.com>
2019-07-22 21:21:42 +0200

    Added README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Memory Maneuver
+This is an implementation for the exercise: [day 8](https://adventofcode.com/2018/day/8)
+
+## How to run it
+Just run `python manage.py`
+
+## How to change the input
+Open the file `input.txt` and change the data
+
+## Requirements
+Python > 3.7
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from models import Node
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
-        input_list = f.read().split(" ")
+        input_list = f.read().strip().split(" ")
         results = list(map(int, input_list))
         root = Node(results, True)
         root.print_tree()
