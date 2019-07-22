1c89bc7642737859851ce0841d0fff5c49932c72
Lendoly <luis.hdezdonadeu@outlook.com>
2019-07-23 01:54:57 +0200

    Added docs

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from models import Node
 
 if __name__ == "__main__":
+    # Opening file and read it
     with open("input.txt", "r") as f:
-        input_list = f.read().strip().split(" ")
-        results = list(map(int, input_list))
-        root = Node(results, True)
-        root.print_tree()
+        try:
+            input_list = f.read().strip().split(" ")
+            results = list(map(int, input_list))
+            root = Node(results, True)
+            root.print_tree()
+        except  ValueError as e:
+            print("Invalid input, probably there are letters on it")
--- models.py
+++ models.py
@@ -2,10 +2,30 @@ import json
 
 
 class Node:
+    """Class that simbolize a Node of a Tree.
 
+    Properties created with the ``@property`` decorator should be documented
+    in the property's getter method.
+
+    Attributes:
+        root (bool): Indicates if is the root Node of a Tree or not
+        number_children (int): Number of children for a Node 
+        amount_of_metada (int): Number of metadat on the node 
+        children (list): List of Nodes which are the children of the Node
+        metadata (list): List of Integers with the metada for the Node 
+    """
     def __init__(self, info_list: list, root: bool = False):
+        """Constructor for the Node
+
+        Note:
+            Do not set root to True on the Tree generation
+
+        Args:
+            info_list (list): list of integers with the input
+            root (bool): indicates if is a ROOT node or not, default to False
+
+        """
         self.root = root
-        self.childs = []
         self.number_children = info_list.pop(0)
         self.amount_of_metada = info_list.pop(0)
         self.generate_children(info_list)
@@ -13,9 +33,28 @@ class Node:
                          for _ in range(self.amount_of_metada)]
 
     def generate_children(self, info_list):
+        """Class method that will generate the chidlren for the Node
+
+        Args:
+            info_list: The rest of the list to generate children.
+
+        Returns:
+            None
+
+        """
+        #: list of Node: caontain the children for the current node
         self.children = [Node(info_list) for _ in range(self.number_children)]
 
     def get_dict(self):
+        """Class method that will generate the children for the Node
+        
+        Args:
+            info_list: The rest of the list to generate children.
+
+        Returns:
+            dict with the information for the 
+
+        """
         data = {"number of children": self.number_children,
                 "amount of metadata": self.amount_of_metada,
                 "metadata": self.metadata,
@@ -26,8 +65,20 @@ class Node:
         return data
 
     def get_total(self):
+        """Class method that will sum the current metada and add the
+        sum of the children's metadata
+        
+        Returns:
+            total amount of the sum of all the metadata 
+
+        """
+
         return sum(self.metadata) + sum(child.get_total()
                                         for child in self.children)
 
     def print_tree(self):
+        """Print in JSON format the tree from the current Node
+        Returns:
+            None
+        """
         print(json.dumps(self.get_dict()))
